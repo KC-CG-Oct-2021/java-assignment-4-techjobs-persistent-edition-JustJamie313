d686b5892fc56a047fe9a62da528e1abac1e11a0
Jamie Miller <dontlaughatme@yahoo.com>
2022-07-06 16:21:05 -0500

    first attempt did not pass TestTaskFour

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,51 +1,40 @@
 package org.launchcode.techjobs.persistent.models;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-public class Job{
-
-    @Id
-    @GeneratedValue
-    private int id;
-
-    private String name;
-
-    private String employer;
-    private String skills;
+public class Job extends AbstractEntity{
+    @ManyToOne
+    private Employer employer;
+    @ManyToMany
+    private final List<Skill> skills = new ArrayList<>();
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer anEmployer) {
         super();
         this.employer = anEmployer;
-        this.skills = someSkills;
     }
 
     // Getters and setters.
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
-        this.skills = skills;
+    public void addSkills(Skill skill) {
+        this.addSkills(skill);
     }
 }
