9661d15ae6cf4e01c4c29f207cbce9d8ffad2beb
Jamie Miller <dontlaughatme@yahoo.com>
2022-07-06 16:44:08 -0500

    Part 1 Steps 1-4 Complete - Test it with SQL #1 complete

--- queries.sql
+++ queries.sql
@@ -1,5 +1,11 @@
 -- Part 1: Test it with SQL
-
+/*
+    COLUMN      DATATYPE
+    id          int PrimaryKey
+    employer    varchar(255)
+    name        varchar(255)
+    skills      varchar(255)
+*/
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
