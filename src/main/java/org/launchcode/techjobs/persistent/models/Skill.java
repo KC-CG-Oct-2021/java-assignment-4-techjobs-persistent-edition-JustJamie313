ba55b0067cf9178e785afab8588f5ba8c9e7c26c
Jamie Miller <dontlaughatme@yahoo.com>
2022-07-06 18:03:23 -0500

    Part 2 AbstractEntity 1,2; Models 1,2;Data Layer 1,2;Controllers 1,2,3,4,5 - complete - Test With SQL Part 2 complete

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,10 +1,13 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Skill extends AbstractEntity {
-
+    @NotBlank(message="Description is required")
+    @Size(max=255,message = "Description must be 255 characters or less")
     private String description;
 
     public Skill(String description) {
