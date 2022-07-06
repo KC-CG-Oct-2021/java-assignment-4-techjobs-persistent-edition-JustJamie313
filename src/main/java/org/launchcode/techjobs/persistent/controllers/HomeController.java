167031f8c75766a5d5e5b40b0041ec71d1717eba
Jamie Miller <dontlaughatme@yahoo.com>
2022-07-06 18:41:04 -0500

    Part 3 Employer 1,2; Job 1,2,3; HomeController 1,2,3,4; Test with SQL Part 3 - complete - job is now stored in database - unable to view/jobid

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -62,7 +62,7 @@ public class HomeController {
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-
+        model.addAttribute("job",jobRepository.findById(jobId));
         return "view";
     }
 
